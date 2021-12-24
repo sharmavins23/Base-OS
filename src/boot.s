59cb54ed6c25c9ad84b8ba573406e1853714cda7
sharmavins23 <sharmavins23@gmail.com>
2021-12-23 22:28:49 -0500

    fixed comments in ARMASM

--- src/boot.s
+++ src/boot.s
@@ -4,24 +4,24 @@
 
 .globl      _start
 _start:
-            mrs         x0, mpidr_el1       ; Pull CPUID out
-            and         x0, x0, #0xFF       ; Verify it is 0xFF
-            cbz         x0, master          ; If it is, branch to master
-            b           proc_hang           ; Otherwise, hang the processor
+            mrs         x0, mpidr_el1       @ Pull CPUID out
+            and         x0, x0, #0xFF       @ Verify it is 0xFF
+            cbz         x0, master          @ If it is, branch to master
+            b           proc_hang           @ Otherwise, hang the processor
 
 master:
             adr         x0, bss_begin
-            adr         x1, bss_end         ; Get the stack space sizing
+            adr         x1, bss_end         @ Get the stack space sizing
             sub         x1, x1, x0
-            bl          memzero             ; Zero the entire memory block
+            bl          memzero             @ Zero the entire memory block
 
-            mov         sp, #LOW_MEMORY     ; Set the stack pointer
-            bl          kernel_main         ; Call kernel.c code
+            mov         sp, #LOW_MEMORY     @ Set the stack pointer
+            bl          kernel_main         @ Call kernel.c code
 
-            b           proc_hang           ; Hang the processor
-                                            ; Since kernel.c loops infinitely,
-                                            ;  this will never happen
+            b           proc_hang           @ Hang the processor
+                                            @ Since kernel.c loops infinitely,
+                                            @  this will never happen
 
 proc_hang:
-            wfe                             ; Wait for an instruction
+            wfe                             @ Wait for an instruction
             b           proc_hang
--- src/utils.s
+++ src/utils.s
@@ -1,20 +1,20 @@
-; Delays the processor for a certain amount of time
-; x0 (u64)  ticks       Tick count of time
+@ Delays the processor for a certain amount of time
+@ x0 (u64)  ticks       Tick count of time
 .globl      delay
-delay:      subs        x0, x0, #1          ; Subtract one tick from x0 count
-            bne         delay               ; If not zero, repeat
+delay:      subs        x0, x0, #1          @ Subtract one tick from x0 count
+            bne         delay               @ If not zero, repeat
             ret
 
-; Places 32'b of data in the specified address
-; x0 (u64)  address     Address to place data in
-; w1 (u32)  value       Value to place
+@ Places 32'b of data in the specified address
+@ x0 (u64)  address     Address to place data in
+@ w1 (u32)  value       Value to place
 .globl      put32
-put32:      str         w1, [x0]            ; Store value w1 in address x0
+put32:      str         w1, [x0]            @ Store value w1 in address x0
             ret
 
-; Gets 32'b of data from the specified address
-; x0 (u64)  address     Address to receive data from
-; w0 (u32)  value       Value return
+@ Gets 32'b of data from the specified address
+@ x0 (u64)  address     Address to receive data from
+@ w0 (u32)  value       Value return
 .globl      get32
-get32:      ldr         w0, [x0]            ; Load value w0 from address x0
+get32:      ldr         w0, [x0]            @ Load value w0 from address x0
             ret
