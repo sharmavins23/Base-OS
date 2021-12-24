f112663288eddc13ecd99fb9b1fd528b7f31db9b
sharmavins23 <sharmavins23@gmail.com>
2021-12-23 21:35:55 -0500

    updated comment formatting

--- src/utils.s
+++ src/utils.s
@@ -1,20 +1,20 @@
 ; Delays the processor for a certain amount of time
-; x0 (u64)  Ticks
+; x0 (u64)  ticks       Tick count of time
 .globl      delay
 delay:      subs        x0, x0, #1          ; Subtract one tick from x0 count
             bne         delay               ; If not zero, repeat
             ret
 
 ; Places 32'b of data in the specified address
-; x0 (u64)  Address
-; w1 (u32)  Value
+; x0 (u64)  address     Address to place data in
+; w1 (u32)  value       Value to place
 .globl      put32
 put32:      str         w1, [x0]            ; Store value w1 in address x0
             ret
 
 ; Gets 32'b of data from the specified address
-; x0 (u64)  Address
-; w0 (u32)  Value
+; x0 (u64)  address     Address to receive data from
+; w0 (u32)  value       Value return
 .globl      get32
 get32:      ldr         w0, [x0]            ; Load value w0 from address x0
             ret
