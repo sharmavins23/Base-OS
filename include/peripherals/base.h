d8073db8831b55327812a1d5cb2a29f3ac9f6232
sharmavins23 <sharmavins23@gmail.com>
2021-12-23 21:04:03 -0500

    added comments for base peripheral addressing info

--- include/peripherals/base.h
+++ include/peripherals/base.h
@@ -1,8 +1,11 @@
 #pragma once
+// Contains base peripheral addresses.
 
+// https://github.com/raspberrypi/linux/tree/rpi-5.4.y/arch/arm/boot/dts/bcm2837.dtsi
 #if RPI_VERSION == 3
 #define PBASE 0x3F000000
 
+// https://github.com/raspberrypi/linux/tree/rpi-5.4.y/arch/arm/boot/dts/bcm2711.dtsi
 #elif RPI_VERSION == 4
 #define PBASE 0xFE000000
 
