db3b479e9b9d967e133a1688a6e33bd7f67f99f9
sharmavins23 <sharmavins23@gmail.com>
2021-12-23 21:35:48 -0500

    added base kernel code

---
+++ src/kernel.c
@@ -0,0 +1,29 @@
+// Contains the kernel's main C code.
+
+#include "common.h"
+#include "mini_uart.h"
+
+// Called from boot ASM file; initializes kernel structure
+void kernel_main() {
+    // Initialize UART functionality
+    uart_init();
+
+    // Send a message on boot to serial
+    uart_send_string("Raspberry Pi Bare Metal OS initializing...\n");
+
+// Serial log board name
+#if RPI_VERSION == 3
+    uart_send_string("\tBoard: Raspberry Pi 3\n");
+#endif
+
+#if RPI_VERSION == 4
+    uart_send_string("\tBoard: Raspberry Pi 4\n");
+#endif
+
+    uart_send_string("\n\nInitialization complete.\n");
+
+    // For now, loop forever and repeat character from UART input
+    while (1) {
+        uart_send(uart_recv());
+    }
+}
