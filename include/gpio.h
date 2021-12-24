6b63ec6a51a6380e3a484910a29927d08d8e3146
sharmavins23 <sharmavins23@gmail.com>
2021-12-23 22:03:31 -0500

    added GPIO control functions

---
+++ include/gpio.h
@@ -0,0 +1,21 @@
+#pragma once
+// User headers for mini UART functionality and function selection
+
+#include "peripherals/gpio.h"
+
+typedef enum _GpioFunc {
+    GFInput = 0,   // GPIO Pin is an input
+    GFOutput = 1,  // GPIO Pin is an output
+    GFAlt0 = 4,    // GPIO Pin takes alternate function 0
+    GFAlt1 = 5,    // GPIO Pin takes alternate function 1
+    GFAlt2 = 6,    // GPIO Pin takes alternate function 2
+    GFAlt3 = 7,    // GPIO Pin takes alternate function 3
+    GFAlt4 = 3,    // GPIO Pin takes alternate function 4
+    GFAlt5 = 2     // GPIO Pin takes alternate function 5
+} GpioFunc;
+
+// Sets a GPIO pin to a specified function
+void gpio_pin_set_func(u8 pinNumber, GpioFunc func);
+
+// Enables a specific GPIO pin
+void gpio_pin_enable(u8 pinNumber);
